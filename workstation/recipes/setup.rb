ecb80ea098359bb7c284da2eaa0bdebd00a18a53
root <root@ip-172-31-31-74.us-east-2.compute.internal>
2017-09-21 05:35:10 +0000

    implemented template

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -2,10 +2,16 @@ package 'ntp' do
   action :install
 end
 
+package 'tree' do
+  action :install
+end
 package 'emacs' do
   action :install
 end
 
-file '/etc/motd' do
-  content "This is chef trainig server"
+template '/etc/motd' do
+  source 'motd.erb'
+  variables(
+  :name => 'technotrainer'
+  )
 end
